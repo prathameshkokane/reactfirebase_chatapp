9d9b64d3e066e369b5ee038f525b9e95d6536dee
Prathamesh Kokane <prathameshkokane8312@gmail.com>
2023-07-09 00:14:07 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDCnR3HSZwjSeV1ODk9JztCSlUzT6sLBd8",
+  authDomain: "chatapp-942dc.firebaseapp.com",
+  projectId: "chatapp-942dc",
+  storageBucket: "chatapp-942dc.appspot.com",
+  messagingSenderId: "308011230844",
+  appId: "1:308011230844:web:1f374b0c09f89e20db0bf9",
+};
+
+export const app = initializeApp(firebaseConfig);
